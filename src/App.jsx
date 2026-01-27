319382e5e55d279b972af2a86dffc744b5473c0d
= <learningsjr@gmail.com>
2026-01-27 12:32:48 +0530

    Fix NotFound import extension removed unnessary import

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
-import AboutMe from "./pages/AboutMe";
 import About from "./pages/About";
 import Experience from "./pages/Experience";
 import Qualifications from "./pages/Qualifications.jsx";
