4dbcb2ecaf8b843f5afed83eeea54f13d8f56f57
= <learningsjr@gmail.com>
2026-08-27 15:38:52 +0530

    certification updated

--- src/data/certification.js
+++ src/data/certification.js
@@ -6,62 +6,67 @@ export const certi = [
   },
   {
     id: 2,
+    label: 'Public Finance: Insurance and Risk Finance for the SDGs',
+    img: 'SDG_Finance_Academy2_ReemaJohn.jpeg',
+  },
+  {
+    id: 3,
     label: 'Career and Education Counselling',
     img: 'career-counselling-certificate_ReemaJohn.png',
   },
   {
-    id: 3,
+    id: 4,
     label:
       'Mental Health Studies - Understanding Behaviour, Burnout and Depression',
     img: 'Alison_Certificate_ReemaJohn.png',
   },
   {
-    id: 4,
+    id: 5,
     label: 'Teaching English: Organising the classroom',
     img: 'classroom_training_bc.jpg',
   },
   {
-    id: 4,
+    id: 6,
     label: 'Digital competences for teaching English',
     img: 'digital_competencesbc.jpg',
   },
   {
-    id: 6,
+    id: 7,
     label: 'English in the multilingual classroom',
     img: 'englishinMultilingualbc.jpg',
   },
   {
-    id: 7,
+    id: 8,
     label: 'Empowering Teachers with Artificial Intelligence (AI)',
     img: 'artificialIntelligence.jpg',
   },
   {
-    id: 8,
+    id: 9,
     label: 'Empowering Teachers with Artificial Intelligence (AI) - Phase II',
     img: 'artificialIntelligence_two.jpg',
   },
   {
-    id: 9,
+    id: 10,
     label: 'Microlearning and Nano Courses to Enhance Teaching-Learning',
     img: 'microlearning.jpg',
   },
   {
-    id: 10,
+    id: 11,
     label: 'Online and Digital Education in the Lens of NEP 2020',
     img: 'onlineAndDigitalEduNEP.jpg',
   },
   {
-    id: 11,
+    id: 12,
     label: 'Open Education Resources (OER): Innovating Education',
     img: 'openEducationResource.jpg',
   },
   {
-    id: 12,
+    id: 13,
     label: 'Innovate & Lead: School Transformation Strategies',
     img: 'transformationStrategies.jpg',
   },
   {
-    id: 13,
+    id: 14,
     label: 'CPD (Continuing Professional Development)',
     img: 'cpd.jpg',
   },
